0a026edb51cc964007b39d1180ae841fcfe8f5c5
jal-morales <jal.morales89@gmail.com>
2024-10-22 11:22:03 -0300

    primer commit

---
+++ app/src/test/java/sumativa/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package sumativa.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
